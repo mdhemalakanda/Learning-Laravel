d516a301ad918c763625381f49247d0cc93734c1
mdhemalakanda <hemal.akanda.39@gmail.com>
2026-09-12 23:54:49 +0600

    Start custom validation lesson with a FormRequest rules class

--- app/Http/Controllers/userInfo.php
+++ app/Http/Controllers/userInfo.php
@@ -2,29 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Service\Logger;
-use App\Service\Notice;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserReqValidate;
 
 class userInfo extends Controller
 {
-    public function user_info(Logger $logger)
+    public function handleUserReq(UserReqValidate $request)
     {
-        $log = $logger->log('Hello this is logger');
-        echo $log;
-    }
-
-    public function get_notice(Notice $notice)
-    {
-        echo $notice->print('Hello its notice');
-    }
-
-    public function handleUserReq(Request $request)
-    {
-        $validated = $request->validate([
-            'username' => ['required', 'string', 'max:255'],
-            'password' => ['required', 'min:6', 'max:50'],
-        ]);
+        $request->rules();
 
         dd($request->all());
     }
