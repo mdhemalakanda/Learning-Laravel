1967b934f2ff89576f0a54985826b7648562be5f
mdhemalakanda <hemal.akanda.39@gmail.com>
2026-09-17 07:50:55 +0600

    Start migration lesson with a create_shops_table migration

---
+++ database/migrations/2026_09_16_161759_create_shops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->comment('Shop Table');
+            $table->id();
+            $table->string('shop_name')->nullable()->comment('Shop Name');
+            $table->integer('shop_number')->nullable();
+            $table->string('shop_address')->nullable();
+            $table->string('shop_phone')->nullable();
+            $table->string('shop_email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
